b7d3977d3f0f382bc3b1d1cad4ca5e2e10a25fe4
Vinay Mannem <v.mannem99@gmail.com>
2020-07-16 13:08:33 -0500

    updated 7/16/2020

--- lib/tennis.dart
+++ lib/tennis.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'BodyLayout2.dart';
 import 'home.dart';
@@ -26,17 +27,18 @@ class _reserveTennisState extends State<reserveTennis> {
   String dropdownValue = 'All Courts';
   bool village = true;
 
-  String _date = "Not set";
-  String _time = "Not set";
-
-  bool dateFilled;
+  bool dateFilled = false;
   bool timeFilled;
   bool lengthFilled;
   bool parkFilled;
   bool numFilled;
 
-  DateTime selectedDate = DateTime.now();
+  int hour;
+  String hourString;
 
+  DateTime _dateTime = DateTime.now();
+  String date = "Date: " + DateTime.now().toString().split(" ")[0].split("-")[1]+ "-" + DateTime.now().toString().split(" ")[0].split("-")[2] + "      " +
+      "Time: " + ((int.parse(DateTime.now().toString().split(" ")[1].split(".")[0].split(":")[0]) > 12) ? ((int.parse(DateTime.now().toString().split(" ")[1].split(".")[0].split(":")[0]) - 12)).toString() + DateTime.now().toString().split(" ")[1].split(".")[0].split(":")[1] + " P.M.": (int.parse(DateTime.now().toString().split(" ")[1].split(".")[0].split(":")[0])).toString()  + ":" +  DateTime.now().toString().split(" ")[1].split(".")[0].split(":")[1] + " A.M.");
 
   @override
   void initState() {
@@ -118,31 +120,159 @@ class _reserveTennisState extends State<reserveTennis> {
   Future back(context) async {
     Navigator.push(context, MaterialPageRoute(builder: (context) => home()));
   }
-  void setCourt() {
+  Widget dateTimePicker(build) {
+    return SizedBox(
+      height: 200,
+      child: CupertinoDatePicker(
+        initialDateTime: _dateTime,
+        onDateTimeChanged: (dateTime) {
+          setState(() {
+            date = "Date: " + dateTime.toString().split(" ")[0].split("-")[1] + "-" + dateTime.toString().split(" ")[0].split("-")[2]  + "     " + "Time: " + dateTime.toString().split(" ")[1].split(".")[0].split(":")[0] + ":" + dateTime.toString().split(" ")[1].split(".")[0].split(":")[1] + " A.M.";
+            if (int.parse(dateTime.toString().split(" ")[1].split(".")[0].split(":")[0]) > 12) {
+              hour = int.parse(dateTime.toString().split(" ")[1].split(".")[0].split(":")[0]) - 12;
+              hourString = hour.toString();
+              date = "Date: " + dateTime.toString().split(" ")[0].split("-")[1] + "-" + dateTime.toString().split(" ")[0].split("-")[2]  + "     " + "Time: " + hourString + ":" + dateTime.toString().split(" ")[1].split(".")[0].split(":")[1] + " P.M.";
 
+            }
+            _dateTime = dateTime;
+            dateFilled = true;
+
+          });
+        },
+      ),
+    );
   }
-  Future<Null> _selectDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime(2015, 8),
-        lastDate: DateTime(2101));
-    if (picked != null && picked != selectedDate)
-      setState(() {
-        selectedDate = picked;
-      });
+  Widget lengthPicker(build) {
+    return SizedBox(
+      height: 200,
+      child: CupertinoDatePicker(
+        initialDateTime: _dateTime,
+        onDateTimeChanged: (dateTime) {
+          setState(() {
+            date = "Date: " + dateTime.toString().split(" ")[0].split("-")[1] + "-" + dateTime.toString().split(" ")[0].split("-")[2]  + "     " + "Time: " + dateTime.toString().split(" ")[1].split(".")[0].split(":")[0] + ":" + dateTime.toString().split(" ")[1].split(".")[0].split(":")[1] + " A.M.";
+            if (int.parse(dateTime.toString().split(" ")[1].split(".")[0].split(":")[0]) > 12) {
+              hour = int.parse(dateTime.toString().split(" ")[1].split(".")[0].split(":")[0]) - 12;
+              hourString = hour.toString();
+              date = "Date: " + dateTime.toString().split(" ")[0].split("-")[1] + "-" + dateTime.toString().split(" ")[0].split("-")[2]  + "     " + "Time: " + hourString + ":" + dateTime.toString().split(" ")[1].split(".")[0].split(":")[1] + " P.M.";
+
+            }
+            _dateTime = dateTime;
+            dateFilled = true;
+
+          });
+        },
+      ),
+    );
+  }
+
+  showAlertDialogDateTime(BuildContext context) {
+    // set up the buttons
+    Widget closeButton = FlatButton(
+      child: Text("Close"),
+      onPressed: () {
+        dateFilled = true;
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+        updateBottomSheet();
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text("Next"),
+      onPressed:  () {
+        dateFilled = true;
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+        updateBottomSheet();
+        showAlertDialogLength(context);
+
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+
+      title: Text("Date and Time"),
+      content: SizedBox(width: MediaQuery.of(context).size.width*.9,child:dateTimePicker(build)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      actions: [
+        closeButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+          width: MediaQuery.of(context).size.width*.9,
+          child: alert,
+        );
+      },
+    );
+  }
+  showAlertDialogLength(BuildContext context) {
+    // set up the buttons
+    Widget closeButton = FlatButton(
+      child: Text("Close"),
+      onPressed: () {
+        dateFilled = true;
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+        updateBottomSheet();
+      },
+    );
+    Widget continueButton = FlatButton(
+      child: Text("Next"),
+      onPressed:  () {
+        dateFilled = true;
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+        updateBottomSheet();
+
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+
+      title: Text("Reservation Length"),
+      content: SizedBox(width: MediaQuery.of(context).size.width*.9,child:dateTimePicker(build)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+      actions: [
+        closeButton,
+        continueButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return SizedBox(
+          width: MediaQuery.of(context).size.width*.9,
+          child: alert,
+        );
+      },
+    );
+  }
+  updateBottomSheet() {
+    Navigator.of(context).pop();
+    showBottomSheet();
+
   }
 
   showBottomSheet() {
     showModalBottomSheet<void>(context: context,
         builder: (BuildContext context) {
+
           return new ListView(
 
             children: <Widget>[
+
               new Row(
+
                 children: [
                   Spacer(),
-                  Text("Create Reservation"),
+                  Center(
+                    child: Text("Create Reservation", style: TextStyle(fontWeight: FontWeight.bold),)
+                  ),
                   Spacer(),
                   IconButton(
                     icon: Icon(Icons.close),
@@ -153,20 +283,13 @@ class _reserveTennisState extends State<reserveTennis> {
               new Card(
                   child: ListTile(
                     leading: new Icon(Icons.calendar_today),
-                    title: new Text("Date"),
+                    title: new Text(dateFilled ? date : "Date"),
                     onTap: () {
-                      _selectDate(context);
-
+                      showAlertDialogDateTime(context);
                     },
                   )
               ),
-              new Card(
-                child: ListTile(
-                  leading: new Icon(Icons.alarm),
-                  title: new Text('Time'),
-                  onTap: (){},
-                ),
-              ),
+
               new Card(
                 child: ListTile(
                   leading: new Icon(Icons.timer),
@@ -207,6 +330,7 @@ class _reserveTennisState extends State<reserveTennis> {
                 ],
               )
             ],
+
           );
         });
   }
